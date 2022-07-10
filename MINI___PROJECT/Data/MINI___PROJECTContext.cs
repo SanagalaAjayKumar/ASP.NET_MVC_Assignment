7ffccf0cc687774d026c89ced27249b28b90cf3a
Ajay Kumar Sanagala <a.sanagala@zensar.com>
2022-07-10 08:36:53 +0530

    Azure_Mini_Project

---
+++ MINI___PROJECT/Data/MINI___PROJECTContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MINI___PROJECT.Models;
+
+namespace MINI___PROJECT.Data
+{
+    public class MINI___PROJECTContext : DbContext
+    {
+        public MINI___PROJECTContext (DbContextOptions<MINI___PROJECTContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MINI___PROJECT.Models.Products> Products { get; set; }
+    }
+}
---
+++ MINI___PROJECT/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MINI___PROJECT.Models
+{
+    public class Products
+    {
+        [Key]
+        public int productID { get; set; }
+        [Required(ErrorMessage = "Please Enter A Product Name ")]
+        public string productName { get; set; }
+        public decimal price { get; set; }
+        public string brand { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
